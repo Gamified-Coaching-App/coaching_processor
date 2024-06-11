0139a031b06350764648c1234acb0f04b7a2d9ab
GabrielOppermann <gabriel.oppermann1997@gmail.com>
2024-06-11 20:45:31 +0200

    just empty implemenation and stack configuration

--- app.mjs
+++ app.mjs
@@ -4,6 +4,33 @@ import { garmin_handler } from './garmin_handler.mjs';
 const app = express();
 app.use(express.json({ limit: '200mb' }));
 
+// /workouts
+// function to orchestrate - 1. if RUN compute HR zones for workout, 
+//2. Add to DB, 
+// 3. prepare data for inference - get data 4. call inference function submitting data and user id
+// 5. call workout builder on received data
+// 6. store workouts in DB
+// 7. convert data to garmin format and send to garmin
+
+// /user-data
+// 1. aggregate subjective parameters
+// 2. call inference function submitting data and user id
+// 3. call workout builder on received data
+// 4. return data to frontend
+// 5. store workouts in DB
+// 6. convert data to garmin format and send to garmin
+
+// /frontend
+// 1. get user data
+// 2. get workouts
+// 3. return data to frontend
+
+// 1 function for training plan computation
+// /health/garmin
+// 1 function for heart rate zone update - triggered if a new summary comes in
+// 1 function to add data to the database of health metrics
+
+
 // POST endpoint that uses the garmin_handler
 app.post('/update/garmin', (req, res) => {
     // Immediately respond to the request
